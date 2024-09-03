076782aadf52dd2dcce7063ee574fac1229d48c4
Lucas Silva <silva.los@outlook.com>
2024-09-03 09:04:05 -0300

    Aula150 Program completed

--- Aula150/src/application/Program.java
+++ Aula150/src/application/Program.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import entities.enums.WorkerLevel;
 
@@ -22,11 +23,11 @@ public class Program {
 		String departmentName = sc.nextLine();
 		
 		System.out.println("Enter worker data: ");
-		System.out.println("Name: ");
+		System.out.print("Name: ");
 		String workerName = sc.nextLine();
-		System.out.println("Level: ");
+		System.out.print("Level: ");
 		String workerLevel = sc.nextLine();
-		System.out.println("Base salary: ");
+		System.out.print("Base salary: ");
 		double workerBaseSalary = sc.nextDouble();
 		sc.nextLine();
 
@@ -40,8 +41,26 @@ public class Program {
 			System.out.println("Enter contrat #" + i + " data:");
 			System.out.print("Date (DD/MM/YYYY): ");
 			Date contractDate = sdf.parse(sc.next());
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
 			
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			
+			worker.addContract(contract);
 		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		
+		System.out.println("Name:  " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for "+ monthAndYear +": " + String.format("%.2f", worker.income(year, month)));
 		
 		
 	}
