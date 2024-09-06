9cdff3a22ccca53661b9ec22b7cf3775664e480b
Lucas Silva <silva.los@outlook.com>
2024-09-06 12:53:37 -0300

    Aprendendo classes abstratas

--- Heranca/src/application/Program.java
+++ Heranca/src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -9,19 +12,34 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		Account acc1 = new Account(1001, "Lucas", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
+		//Account acc1 = new Account(1001, "Lucas", 1000.0);	
 		
+		List<Account> list = new ArrayList<>();
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.05);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		list.add(new SavingsAccount(1001, "Maria", 10000.0, 0.05));
+		list.add(new SavingsAccount(1002, "Lucas", 10.0, 0.05));
+		list.add(new SavingsAccount(1003, "Claudio", 10000.0, 0.05));
 		
-		Account acc3 = new BusinessAccount(1002, "Maria", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
+		list.add(new BusinessAccount(1004, "Matheus", 100.0, 500.0));
+		list.add(new BusinessAccount(1005, "Lucimara", 110000.0, 6000.0));
+		list.add(new BusinessAccount(1006, "Thamires", 1000.0, 0.1));
 	
+		
+		double sum = 0.0;
+		for (Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		
+		for (Account acc : list) {
+			acc.deposit(10);
+			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
+
+		
+		
 	}
 
 }
