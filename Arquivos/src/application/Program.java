000bbd4f2a59a8ede2319ac68295da2fac41a8b8
Lucas Silva <silva.los@outlook.com>
2024-09-11 08:11:13 -0300

    Try with resources

--- Arquivos/src/application/Program.java
+++ Arquivos/src/application/Program.java
@@ -10,12 +10,8 @@ public class Program {
 		
 		String path = "C:\\Windows\\Temp\\in.txt";
 		
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			String line = br.readLine();
 			
@@ -28,19 +24,7 @@ public class Program {
 		catch (IOException e){
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 
 	}
 
